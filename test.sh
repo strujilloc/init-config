b95c496c33704f0ac1d644ea96a9d3474d1c1b88
saultc <saul@s.h.k.u-tokyo.ac.jp>
2020-04-26 00:45:37 +0900

    min update

--- test.sh
+++ test.sh
@@ -1,6 +1,2 @@
-#!/usr/bin/env zsh
-
-plugins=(git z)
-sed -i "s/plugins=(${plugins})/plugins=(${plugins} newplug)/" test.txt
-plugins+=(newplug)
-sed -i "s/plugins=(${plugins})/plugins=(${plugins} 2ndplug)/" test.txt
+#!/bin/bash
+env
--- ubuntu-init.sh
+++ ubuntu-init.sh
@@ -16,8 +16,6 @@ inst_tools(){
 inst_speedtest(){
     sudo apt install gnupg1 apt-transport-https dirmngr -y
     export INSTALL_KEY=379CE192D401AB61
-    # Ubuntu versions supported: xenial, bionic
-    # Debian versions supported: jessie, stretch, buster
     export DEB_DISTRO=$(lsb_release -sc)
     sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys $INSTALL_KEY
     echo "deb https://ookla.bintray.com/debian ${DEB_DISTRO} main" | sudo tee  /etc/apt/sources.list.d/speedtest.list
