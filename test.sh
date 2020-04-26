fabcd39ab265de55a190dd4f6f4ee67942e5ea4f
saultc <saul@s.h.k.u-tokyo.ac.jp>
2020-04-26 13:29:47 +0900

    some test about function zsh theme

--- test.sh
+++ test.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-env
+echo -e '\n# Function - change zsh theme'>> ~/.zshrc
+echo 'zshtheme(){
+    new_ZSH_THEME=$1
+    sed -i "s/ZSH_THEME=${ZSH_THEME}/ZSH_THEME=${new_ZSH_THEME}/" ~/.zshrc
+}' >> ~/.zshrc
